dee1302d7187cef35c3f4e902a9574ca183587b1
Rehan Manzoor <caprichano@gmail.com>
2017-03-27 23:44:40 +0400

    working with meteor foundation

--- imports/api/Users.js
+++ imports/api/Users.js
@@ -1,10 +1,20 @@
-import { Meteor } from 'meteor/meteor';
-import { Mongo } from 'meteor/mongo';
+import {Meteor} from 'meteor/meteor';
+import {Mongo} from 'meteor/mongo';
+import {check} from 'meteor/check';
 
 export const Users = new Mongo.Collection('users');
 
-if( Meteor.isServer ){
-    Meteor.publish('users', function userPublication(){
+if (Meteor.isServer) {
+    Meteor.publish('users', function userPublication() {
         return Users.find({});
     });
 }
+
+
+Meteor.methods({
+    'users.insert'({name, age}){
+        Users.insert({
+            name, age
+        });
+    }
+});
--- imports/ui/CreateUsers.jsx
+++ imports/ui/CreateUsers.jsx
@@ -1,50 +1,72 @@
 import React, {Component} from 'react';
-
+import {Meteor} from 'meteor/meteor';
 
 class CreateUsers extends Component {
     constructor() {
         super();
 
         this.state = {
-            name: '',
-            age: '',
-            btnText:'Create User',
+            user: {
+                name: '',
+                age: '',
+            },
+            btnText: 'Create User',
             btnEnabled: true
         };
     }
 
     handleChange(e) {
-        const {key, value} = e.target;
+        const {name, value} = e.target;
 
-        this.setState({key: value});
+        const {user} = this.state;
+        user[name] = value;
+
+        this.setState({user});
     }
 
-    enableButton(){
-
+    enableButton() {
+        this.setState({btnText: 'Create User', btnEnabled: true});
     }
-    
-    disableButton(){
 
+    disableButton() {
+        this.setState({btnText: 'Creating User...', btnEnabled: false});
+    }
+
+    createUser() {
+        let {user} = this.state;
+        this.disableButton();
+
+        Meteor.call('users.insert', user);
+
+        user.name = '';
+        user.age = '';
+
+        this.setState({user});
+
+        this.enableButton();
     }
 
     render() {
+        const {btnText, btnEnabled, user} = this.state;
         return (
             <div className="row">
                 <div className="col-lg-6 col-lg-offset-3">
 
                     <div className="form-group">
                         <label htmlFor="">Name</label>
-                        <input type="text" name="name" className="form-control"
+                        <input type="text" name="name" className="form-control" value={user.name}
                                onChange={this.handleChange.bind(this)}/>
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="">Age</label>
-                        <input type="text" name="age" className="form-control" onChange={this.handleChange.bind(this)}/>
+                        <input type="text" name="age" className="form-control" value={user.age}
+                               onChange={this.handleChange.bind(this)}/>
                     </div>
 
                     <div className="form-group">
-                        <input type="button" className="btn btn-primary btn-sm" value="Create User" />
+                        <input type="button" className="btn btn-primary btn-sm" onClick={this.createUser.bind(this)}
+                               value={btnText} disabled={!btnEnabled}/>
                     </div>
 
                 </div>
